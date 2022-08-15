0da39dd5bbb1466d8f790e5e32ffb85d30071f27
Ece Çakmak <86318154+ececmk@users.noreply.github.com>
2022-08-15 09:30:00 +0200

    done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,55 @@
-// Iteration #1
+const express = require('express');
+const router = express.Router();
+
+// require the Drone model here
+const Drone = require('../models/Drone.model');
+
+router.get('/drones', (req, res, next) => {
+  router.get('/', (req, res) => {
+    Drone.find().then((drones) => {
+      res.render('drones/list', { drones });
+    });
+  });
+  
+});
+
+router.get('/drones/create', (req, res, next) => {
+  res.render("drones/create-form")
+});
+
+router.post('/drones/create', (req, res, next) => {
+  // Iteration #3: Add a new drone
+  const droneDetails = {
+    name: req.body.name,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed
+  };
+
+  Drone.create(droneDetails)
+  .then(() => {
+    res.redirect("/drones")
+  })
+  .catch((error) => console.log("Error creating new drone", error))
+});
+
+router.post('/drones/:id/edit', (req, res, next) => {
+  // Iteration #4: Update the drone
+  const droneId = req.params.id;
+  const newDetails = {
+    name: req.body.name,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed
+  };
+  Drone.findByIdAndUpdate(droneId, newDetails)
+  .then(() => res.redirect("/drones"))
+  .catch((error) => console.log("Error editing drone", error))
+});
+
+router.post('/drones/:id/delete', (req, res, next) => {
+  // Iteration #5: Delete the drone
+  Drone.findByIdAndDelete(req.params.id)
+  .then( () => {res.redirect("/drones")})
+  .catch((error) => console.log("Error deleting drone", error))
+});
+
+module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,31 @@
 // Iteration #1
+const DRONE = require('../data/drone.json')
+const Drone = require('../models/Drone.model')
+
+require('../config/db.config')
+
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+    { name: "Josh-800", propellers: 2, maxSpeed: 31 },
+    { name: "Jaime v5", propellers: 4, maxSpeed: 32 },
+    { name: "Lloyd-Ultra", propellers: 99, maxSpeed: 9000 },
+    { name: "Ironhack M1 Lightyear", propellers: Infinity , maxSpeed: Infinity }
+    
+  ];
+
+  mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    return Drone.create(drones)
+  })
+  .then( (drones) => {
+     console.log(`created ${drones.length} drones`);
+     mongoose.connection.close();
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
